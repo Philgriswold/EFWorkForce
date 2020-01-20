a633eaed1620f04b2472d8de1756b48a5f91dda4
Phil Griswold <philgriswold@me.com>
2020-01-20 13:24:16 -0800

    first commit

---
+++ EFWorkForce/Models/Employee.cs
@@ -0,0 +1,43 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace EFWorkforce.Models
+
+{
+
+    public class Employee
+
+    {
+
+        public int Id { get; set; }
+
+
+
+        [Display(Name = "Name")]
+        [Required]
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public string Email { get; set; }
+
+        public bool IsSupervisor { get; set; }
+
+        public int ComputerId { get; set; }
+        public Computer Computer { get; set; }
+
+        public Department Department { get; set; }
+
+    }
+
+}
